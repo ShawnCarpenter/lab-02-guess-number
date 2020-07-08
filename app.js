f37411db7c199e22b318cbe0732c60a77e0c6ca5
Shawn Carpenter <shawn.carpeenter@gmail.com>
2020-07-07 17:18:06 -0700

    the game is working, after a win or loss the button changes to restart

--- app.js
+++ app.js
@@ -1,11 +1,61 @@
 // import functions and grab DOM elements
-import { compareNumbers } from './guessUtils.js';
+import { compareNumbers, generateNumber } from './guessUtils.js';
 const playButton = document.getElementById('play-button');
-
+const guess = document.getElementById('guess');
+const scoreboardWins = document.getElementById('wins');
+const scoreboardLosses = document.getElementById('losses');
+const scoreboardLastGuess = document.getElementById('hi-low');
+const scoreboardTriesRemaining = document.getElementById('tries-remaining');
 // initialize state
-
+let correctNumber = generateNumber();
 let wonGames = 0;
 let lostGames = 0;
 let triesRemaining = 4;
-
+let gameOver = false;
 // set event listeners to update state and DOM
+
+playButton.addEventListener('click', ()=> {
+    let lastGuess = '';
+    if (!gameOver) {
+        const guessedNumber = Number(guess.value);
+        switch (compareNumbers(guessedNumber, correctNumber)) {
+            case 1 :
+                triesRemaining--;
+                lastGuess = 'too high';
+                break;
+            case 0 :
+                wonGames++;
+                lastGuess = 'correct';
+                gameOver = true;
+                break;
+            case -1 :
+                triesRemaining--;
+                lastGuess = 'too low';
+                break;
+        }
+    } else {
+        triesRemaining = 4;
+        gameOver = false;
+        correctNumber = generateNumber();
+        playButton.textContent = 'Try your luck';
+    }
+    
+    // make sure we aren't out of guesses.
+    if (triesRemaining === 0) {
+        lostGames++;
+        gameOver = true;
+    }
+    //update the scoreboard
+    scoreboardLastGuess.textContent = lastGuess;
+    scoreboardTriesRemaining.textContent = triesRemaining;
+    scoreboardWins.textContent = wonGames;
+    scoreboardLosses.textContent = lostGames;
+    //reset the game
+    if (gameOver) {
+        playButton.textContent = 'Restart';
+    }
+
+
+
+    
+});
--- guessUtils.js
+++ guessUtils.js
@@ -1,8 +1,12 @@
 //Function compareNumbers needs to return -1 for a low guess, 0 for a correct guess, 1 for a high guess
 
 export function compareNumbers(guess, correctNumber) {
-    
+    const guessedNumber = Number(guess);
     if (guess === correctNumber) return 0;
     if (guess > correctNumber) return 1;
     if (guess < correctNumber) return -1;
+}
+
+export function generateNumber() {
+    return Math.ceil(Math.random() * 20);
 }
